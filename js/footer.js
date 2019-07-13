d5863525349e109e900018af27c23171db2648d2
Feng Hongan <huaan853@gmail.com>
2019-07-13 17:53:45 +0800

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,12 @@
 // JavaScript Document
+//	根據當前時間自動更改版權年份
+//
 function footer()
 {
+	var myDate = new Date();
+	var year= myDate.getFullYear();
   document.write("<footer class='footer'>");
-  document.write("<p>&copy; 2019 澳門高血壓聯盟</p>");
+  document.write("<p>&copy;" + year.toString() +" 澳門高血壓聯盟</p>");
   document.write("</footer>");
 	
 }
--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,25 @@ function MenuItem(fileName,desc)
 	this.fileName=fileName;
 	this.desc=desc;
 }
+function getRefPath()
+{
+	var cref=document.URL;
+	var js=document.scripts;
+	var jsPath=js[0].src.substring(0,js[0].src.lastIndexOf("/"));;
+	var root=jsPath.substring(0,jsPath.lastIndexOf('/'));
+	var path=cref.substring(0,cref.lastIndexOf('/'));
+	var rel='';
+	if(root != path){
+		rel='../';
+	}
+	return rel;
+}
+//	由於button屬性不能被chrome接受，不能設置button全部屬性
+//	只好在html保留button
 function menu(active)
 {
+	var a=document.getElementsByTagName("nav");
+	var na=a[0];
 	var index= new MenuItem("index","首頁");	
 	var constitution= new MenuItem("constitution","聯盟章程");
 	var about= new MenuItem("about","聯盟簡介");
@@ -15,20 +32,31 @@ function menu(active)
 	var healthKnowledge= new MenuItem("healthKnowledge","健康教育");
 	var register= new MenuItem("register","會員登記");
 	var items =[index,constitution,about,news,activity,research,healthKnowledge,register];
-    document.write("<ul class='navbar-nav mr-auto justify-content-between' style='width:100%'>");
+	var div = document.createElement('div');
+	div.className="navbar-collapse collapse";
+	div.id="navbarsExampleDefault";
+	div.style='';
+ 	var ul = document.createElement('ul');
+	ul.className="navbar-nav mr-auto justify-content-between";
+	ul.style="width:100%";
+	var rel=getRefPath();
 	for(i=0;i<items.length;i++){
-        document.write("<li class='nav-item'> <a class='nav-link");
-		if(items[i].fileName==active){
-			document.write(" active");	
+		var li = document.createElement('li');
+		var a = document.createElement('a');
+		li.className="nav-item";
+		a.className="nav-link";
+		a.href=rel + items[i].fileName + '.html';
+		a.textContent=items[i].desc;
+		if(items[i].desc == active){
+			li.className +=" active";
+			var span1 = document.createElement('span');
+			span1.className="sr-only";
+			span1.text='(current)';
+			a.appendChild(span1);
 		}
-		document.write("' href='../");
-		document.write(items[i].fileName);
-		document.write(".html'>");
-		document.write(items[i].desc);
-		if(items[i].fileName==active){
-			document.write("<span class='sr-only'>(current)</span>");	
-		}
-		document.write("</a> </li>");
+		li.appendChild(a);
+		ul.appendChild(li);
 	}
-    document.write("</ul>");	
+	div.appendChild(ul);
+	na.appendChild(div);
 }
