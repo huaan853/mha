8bd78ef019932cc259ed50f2277e967b6770288c
Feng Hongan <huaan853@gmail.com>
2020-10-13 23:10:48 +0800

    Add files via upload

--- js/activity.js
+++ js/activity.js
@@ -25,6 +25,9 @@ function yearCount(year)
 	case 2019:
 		cnt=2;
 		break;
+	case 	2020:
+		cnt=11;
+		break;
 	}
 	return cnt;
 }
@@ -41,4 +44,39 @@ function activity(year)
 		document.write("<img class='card-img-top' src='../img/photo/w450" + fn + "_450.jpg' style='width: 100%; display: block;' alt='" + year +"年世界高血壓日'>");
 		document.write("</a> </div></div>");		
 	}
+}
+
+//	2020.10.13
+//
+function singleActivity()
+{
+	var cs=document.currentScript;
+	var curParent=cs.parentNode;
+	var div1 = document.createElement('div');
+	div1.className="col-md-4";
+	var div2 = document.createElement('div');
+	div2.className="card mb-4 box-shadow";
+	
+}
+//	
+function mainActivity(url,h,d)
+{
+	var cs=document.currentScript;
+	var curParent=cs.parentNode;
+	var anchor = document.createElement('a');
+	anchor.className="list-group-item list-group-item-action flex-column align-items-start";
+	anchor.href="activity/" + url +".html";
+	var div = document.createElement('div');
+	div.className="d-flex w-100 justify-content-between";
+	var h6 = document.createElement('h6');
+	h6.className="mb-1";
+	h6.textContent=h;
+	div.appendChild(h6);
+	var p = document.createElement('p');
+	p.className="mb-1";
+	p.textContent=d;
+	anchor.appendChild(div);
+	anchor.appendChild(p);
+	curParent.appendChild(anchor);	
+	
 }
